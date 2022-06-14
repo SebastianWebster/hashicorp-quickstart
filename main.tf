05ba9badc7192f609a14239c639fe6cfd40f02fd
SebastianWebster <38852717+SebastianWebster@users.noreply.github.com>
2022-06-14 22:55:21 +1000

    moved tf providers config to seperate file

--- main.tf
+++ main.tf
@@ -1,26 +1,3 @@
-terraform {
-  cloud {
-    organization = "sw-test-org"
-    workspaces {
-      name = "Example-Workspace"
-    }
-  }
-
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "ap-southeast-2"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0c6120f461d6b39e9"
   instance_type = "t3.micro"
---
+++ providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "sw-test-org"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
